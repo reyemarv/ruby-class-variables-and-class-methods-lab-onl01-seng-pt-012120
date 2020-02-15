303882b9d986e2e7403237caf68f3fda3f5f736e
Marvin Reyes <reyemarv@gmail.com>
2020-02-15 17:12:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,10 @@ class Song
   end
   
   def self.genre_count
-    genre_hash[@@genres]
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
   end 
 
 end 
