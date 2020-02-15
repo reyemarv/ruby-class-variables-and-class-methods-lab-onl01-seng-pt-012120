08155674d684311530a8b90c2c95fec3b60b283a
Marvin Reyes <reyemarv@gmail.com>
2020-02-15 17:28:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,10 +27,16 @@ class Song
     @@artists.uniq
   end
   
-  def self.genre_count
+def self.genre_count
     genre_count = {}
-    @@genres.each { |genre| genre_count[genre] += 1 }
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
     genre_count
-  end 
+  end
 
 end 
